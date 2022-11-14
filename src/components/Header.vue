d76bc56988f63faaf28a21bd5dfeefe0c19f6eef
Siphosethu Dlomo <udlomo95@gmail.com>
2022-11-14 23:53:38 +0200

    added background gradient to the header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,46 +1,44 @@
 <template>
-  <header class="container-fluid">
-    <div class="container-xxl">
-     <nav class="navbar navbar-expand-md ">
-      <div class="navbar-brand">
-        <router-link :to="{ name: 'Home'}"><img src="" alt="" />Logo Here</router-link>
-      </div>
+  <header class="vh-100">
+    
+    
+      <nav class="navbar navbar-expand-md ">
+        <div class="navbar-brand">
+          <router-link :to="{ name: 'Home'}"><img src="" alt="" />Logo Here</router-link>
+        </div>
 
-      <!-- toggle button for mobile nav -->
-      <button 
-        class="navbar-toggler text-white" 
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#main-nav"
-        aria-controls="main-nav"
-        aria-expanded="false"
-        aria-label="Toggle-navigation"
-      >
-        <span class="navbar-toggler-icon bg-primary"></span>
-      </button>
+        <!-- toggle button for mobile nav -->
+        <button 
+          class="navbar-toggler text-white" 
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#main-nav"
+          aria-controls="main-nav"
+          aria-expanded="false"
+          aria-label="Toggle-navigation"
+        >
+          <span class="navbar-toggler-icon bg-primary"></span>
+        </button>
 
-      <!-- navbar links -->
-      <div
-        class="collapse navbar-collapse justify-content-end align-center"
-        id="main-nav"
-      >
-
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <router-link class="nav-link text-white" :to="{ hash: '#About'}">About</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link class="nav-link text-white" :to="{ hash: '#Follow'}">Follow Us</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link class="nav-link text-white" :to="{ hash: '#Bookings'}">Bookings</router-link>
-          </li>
-        </ul>
-
-      </div>
+        <!-- navbar links -->
+        <div class="collapse navbar-collapse justify-content-end align-center" id="main-nav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <router-link class="nav-link text-white" :to="{ hash: '#About'}">About</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link text-white" :to="{ hash: '#Follow'}">Follow Us</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link text-white" :to="{ hash: '#Bookings'}">Bookings</router-link>
+            </li>
+          </ul>
+        </div>
 
       </nav>
-    </div>
+
+      <h1 class="hero-heading-text text-white">G. Centric Music</h1>
+       
   </header>
 </template>
 
@@ -55,10 +53,18 @@
 <style lang="scss" scoped>
 
   header {
-    background-image: url(../assets/images/header.png);
-    background-size:cover;
-    background-position:center;
-    height: 100vh;
+    // background-image: url(../assets/images/header.png);
+    // background-size:cover;
+    // background-position:center;
+    background: rgb(255,51,0);
+    background: linear-gradient(106deg, rgba(255,51,0,0.80) 50%, rgba(226,192,90,1) 100%);
+
+    // height: 100vh;
+
+    .hero-img {
+      background: rgb(255,51,0);
+      background: linear-gradient(106deg, rgba(255,51,0,0.80) 50%, rgba(226,192,90,1) 100%);
+    }
   }
 
 </style>
