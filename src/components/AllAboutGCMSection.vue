6baddbfcd8a49bd37dc75c7f6fdf2db87cd7a86d
Siphosethu Dlomo <udlomo95@gmail.com>
2022-11-15 17:56:42 +0200

    modified booings section

--- src/components/AllAboutGCMSection.vue
+++ src/components/AllAboutGCMSection.vue
@@ -49,16 +49,19 @@
               </div>
               <div class="py-1">
                 <!-- add mailto: -->
-                <i class="bi bi-envelope me-1 fs-1"></i>
+                <!-- <i class="bi bi-envelope me-1 g"></i> -->
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope me-1 text-white" viewBox="0 0 16 16">
+                <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"/>
+              	</svg>
                 <span class="artist-email">thulanisitho@gmail.com</span>
               </div>
               <div class="pb-1">
                 <!-- add telephone attribute <a href="tel:+4733378901">+47 333 78 901</a> -->
-                <i class="bi bi-whatsapp me-1 fs-5"></i>
+                <i class="bi bi-whatsapp me-1 g"></i>
                 <span class="artist-number artist-whatsapp-number">067 806 8671</span>
               </div>
               <div class="">
-                <i class="bi bi-phone me-1 fs-4"></i>
+                <i class="bi bi-phone me-1 g"></i>
                 <span class="artist-number artist-phonecall-number">067 933 5610</span>
               </div>
             </div>
@@ -119,6 +122,10 @@
 
     .gcm-sm-facebook {
       font-size: 2.3rem;
+    }
+
+    .g {
+      font-size: 5vw;;
     }
   }
 </style>
